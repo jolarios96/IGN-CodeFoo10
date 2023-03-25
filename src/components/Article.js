b0e928a0125e1ec07c71f1451fcba175fa6a9abe
Jordan Larios <jolarios96@gmail.com>
2023-03-25 04:03:02 -0700

    changed Item.js to Article.js for consistency

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,19 +1,19 @@
 import PropTypes from "prop-types";
 
-function Item(props) {
+function Article(props) {
   return (
-    <article className="Item">
+    <article className="Article">
       {props.children ? props.children : <h2>{props.content}</h2>}
     </article>
   );
 }
 
-Item.defaultProps = {
-  content: "Placeholder Item",
+Article.defaultProps = {
+  content: "Placeholder Article",
 };
 
-Item.propTypes = {
+Article.propTypes = {
   content: PropTypes.string,
 };
 
-export default Item;
+export default Article;
