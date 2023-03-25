9b8f1c740f639b22d60678cbd72acc8e2aadcd5c
Jordan Larios <jolarios96@gmail.com>
2023-03-25 03:48:23 -0700

    update layout and syntax

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 // import "./App.css";
 import Poll from "./components/Poll";
-// import Item from "./components/Item";
+import Item from "./components/Item";
 
-function App() {
+function Main() {
   return (
-    <div className="App">
-      <Poll />
-    </div>
+    <main className="Main">
+      <Item />
+      <Item>
+        <h2>This is an Article containing a Poll.</h2>
+        <p>This is the Article's description.</p>
+        <Poll
+          title="Poll Title"
+          query="Pick a choice."
+          choices={["Choice A", "Choice B", "Choice C", "Choice D"]}
+        />
+      </Item>
+      <Item />
+      <Item>
+        <h1>This Article contains stuff.</h1>
+        <p>This is the Article's description.</p>
+      </Item>
+    </main>
   );
 }
 
-export default App;
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Main from "./App";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Header />
-    <App />
+    <Main />
     <Footer />
   </React.StrictMode>
 );
