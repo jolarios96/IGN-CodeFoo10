5ef87e25325c1488a629b4cba360ff1b4642024c
Jordan Larios <jolarios96@gmail.com>
2023-03-31 05:07:44 -0700

    moved & integrated component references into App.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-import App from "./App";
+import App from "./App.js";
 import "./index.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals.js";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
@@ -16,11 +12,10 @@ const client = new ApolloClient({
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <ApolloProvider client={client}>
-    <Header />
     <App />
-    <Footer />
   </ApolloProvider>
 );
 
